cfbb0de066d94954351fce6609a2fe3a09ba54d6
amaranaseem <ammarahnaseem2431@gmail.com>
2025-06-20 03:50:44 +0500

    App file updated with BottomNavTab and homescreen removed to avoid duplicates

--- App.js
+++ App.js
@@ -2,22 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import LoginScreen from './screens/LoginScreen';
 import SplashScreen from './screens/SplashScreen';
 import BoardingScreen from './screens/BoardingScreen';
+import BottomNavTab from './BottomNavTab';
 
 
 
 
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-      <Stack.Screen name="Home" component={HomeScreen}/>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown: false}}>
+      <Stack.Screen name="HomeTabs" component={BottomNavTab}/>  
       <Stack.Screen name="Register" component={RegisterScreen}/>
       <Stack.Screen name="Login" component={LoginScreen}/>
       <Stack.Screen name="Splash" component={SplashScreen}/>
