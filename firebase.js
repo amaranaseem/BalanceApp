4a47c80f04618fa8d300c592f47087c6039a2f4d
amaranaseem <ammarahnaseem2431@gmail.com>
2025-06-30 07:22:43 +0500

    Android folder added to view the app on android device

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLRUrBSbk4N7Ey0p6dHZIXdjO0xHjNn50",
+  authDomain: "balanceapp-3df44.firebaseapp.com",
+  projectId: "balanceapp-3df44",
+  storageBucket: "balanceapp-3df44.appspot.com",
+  messagingSenderId: "944505135387",
+  appId: "1:944505135387:web:bc5e258c1637b7120cf3be",
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
