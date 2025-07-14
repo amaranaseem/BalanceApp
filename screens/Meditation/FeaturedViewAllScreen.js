d4252ba5ce7fddb01cf52965fd82f71d877b15bb
amaranaseem <ammarahnaseem2431@gmail.com>
2025-07-14 07:40:15 +0500

    Page for viewing all the featured meditation list created.

---
+++ screens/Meditation/FeaturedViewAllScreen.js
@@ -0,0 +1,133 @@
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { collection, getDocs, query } from 'firebase/firestore';
+import { db, auth } from '../../firebase';
+import { useFocusEffect } from '@react-navigation/native';
+
+export default function MeditationScreen({ navigation }) {
+const [featuredMeditation, setFeaturedMeditation] = useState([]); 
+const [loading, setLoading] = useState(true);
+
+useFocusEffect(
+    useCallback(() => {
+      const fetchFeaturedMeditation = async () => {
+        try {
+          const featuredRef = collection(db, 'featuredMeditation');
+          const snapshot = await getDocs(featuredRef);
+
+          const fetchMeditations = snapshot.docs.map((doc) => {
+            const data = doc.data();
+            return {
+              id: doc.id,
+              title: data.title,
+              duration: data.duration, 
+              url: data.url,
+              isFeatured: data.isFeatured,
+              imgURL: data.imgURL,
+            };
+          });
+
+          setFeaturedMeditation(fetchMeditations);
+        } catch (error) {
+          console.error('Error fetching featured meditation:', error);
+        } finally {
+          setLoading(false);
+        } 
+      };
+
+      fetchFeaturedMeditation();
+    }, [])
+);
+
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.topRow}>
+        <Text style={styles.headerText}>Featured Meditation</Text>
+        <TouchableOpacity style={styles.closeCircle} onPress={() => navigation.navigate('BottomNavTab', {screen: 'Meditate'})}>
+          <Ionicons name="close" size={22} color="black" />
+        </TouchableOpacity>
+      </View>
+
+      {/* Featured Meditation */}
+      <ScrollView>
+      {featuredMeditation.map((item, index) => (
+        <TouchableOpacity key={item.id || index} 
+        style={styles.featuredItem} 
+        onPress={() => navigation.navigate('AudioPlayerScreen', { 
+        title: item.title,
+        url: item.url, 
+        imgURL: item.imgURL,
+        }
+      )}
+      >
+          <View>
+            <Text style={styles.featuredTitle}>{item.title}</Text>
+            <Text style={styles.featuredSubtitle}>{item.duration}</Text>
+          </View>
+          <Ionicons name="play-circle-outline" size={28} color="#50483D" />
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  paddingTop: 60,
+  paddingHorizontal: 20,
+  paddingVertical: 20,
+  backgroundColor: '#FAF9F6',
+},
+
+topRow: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  gap: 12,
+  marginBottom: 40,
+},
+
+closeCircle: {
+  width: 38,
+  height: 38,
+  borderRadius: 19,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#D8CAB8',
+  opacity: 0.8,
+},
+
+headerText: {
+  fontSize: 26,
+  fontWeight: 'bold',
+  color: '#50483D',
+},
+  
+featuredItem: {
+  backgroundColor: '#EFE5E7',
+  borderRadius: 15,
+  padding: 16,
+  marginBottom: 12,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+},
+
+featuredTitle: {
+  fontSize: 16,
+  fontWeight: '500',
+   color: '#000',
+},
+
+featuredSubtitle: {
+  fontSize: 14,
+  color: '#666',
+  marginTop: 4,
+   color: '#000',
+},
+
+});
